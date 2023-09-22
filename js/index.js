13cab46851be81972431844b85444a67eaaedcb6
Harsh <73895659+iharsh02@users.noreply.github.com>
2023-09-22 16:57:21 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let result = {
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "senpaipluxury",
+    "email": "senpaipluxury@gmail.com",
+    "score": 0.48,
+    "state": "undeliverable",
+    "domain": "gmail.com",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
+  }
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_rsJUn98QJebklcZPngvOiQBGqJhglmJhCKPYZcDw"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
